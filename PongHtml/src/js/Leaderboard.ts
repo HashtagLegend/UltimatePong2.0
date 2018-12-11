2ae085944af65be29c38b12ed9a3285394dd56d3
Jesper Tim Damgaard Krøyer <jesp9030@edu.easj.dk>
2018-12-11 12:27:20 +0100

    Fixet edit bruger og puttet numrering på leaderboard

--- PongHtml/src/js/Leaderboard.ts
+++ PongHtml/src/js/Leaderboard.ts
@@ -30,22 +30,29 @@ constructor(){
                 
                 if((a.wins / a.loses) < (b.wins / b.loses))
                     return 1;
+                if(isNaN(b.wins / b.loses))
+                {
+                    return -1;
+                }
                 return 0;
                 }
             )
             listOfUser.forEach(listUser => {
-                this.insertIntoTable(listUser);
+                this.insertIntoTable(listUser, listOfUser.indexOf(listUser));
             })
         })
         .catch((error:AxiosError):void => {
         })
     }
 
-    private insertIntoTable(user: User){
+    private insertIntoTable(user: User, index: number){
         let newRow = this.leaderBoard.insertRow(-1);
-        let iDCell = newRow.insertCell(0);
-        let usernameCell = newRow.insertCell(1);
-        let wDR = newRow.insertCell(2);
+        let indexCell = newRow.insertCell(0);
+        let iDCell = newRow.insertCell(1);
+        let usernameCell = newRow.insertCell(2);
+        let wDR = newRow.insertCell(3);
+        indexCell.appendChild(document.createTextNode(index.toString()));
+        indexCell.setAttribute('class', 'ProfileStats');
         iDCell.appendChild(document.createTextNode(user.id.toString()));
         iDCell.setAttribute('class', 'ProfileStats');
         usernameCell.appendChild(document.createTextNode(user.brugernavn));
